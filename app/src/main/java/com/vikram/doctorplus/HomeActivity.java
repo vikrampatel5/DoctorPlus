f1a9e3f48f55a3ed0039eee40c65b1af4a0ebf6d
Vikram Patel <vikrampatel5@gmail.com>
2017-09-14 08:21:39 +0530

    Added Login Activity.

--- app/src/main/java/com/vikram/doctorplus/HomeActivity.java
+++ app/src/main/java/com/vikram/doctorplus/HomeActivity.java
@@ -1,8 +1,12 @@
 package com.vikram.doctorplus;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,15 +15,27 @@ import com.google.firebase.auth.FirebaseAuth;
 public class HomeActivity extends AppCompatActivity {
 
     Button logoutButton;
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        navigationView = (NavigationView)findViewById(R.id.nav);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+
+        drawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();;
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         logoutButton = (Button)findViewById(R.id.logout);
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v){
                 logout();
             }
         });
@@ -29,5 +45,13 @@ public class HomeActivity extends AppCompatActivity {
         FirebaseAuth.getInstance().signOut();
         startActivity(new Intent(HomeActivity.this,RegisterActivity.class));
         finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
